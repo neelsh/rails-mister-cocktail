9d71e2b3db67e3e75e0f6bbf5b1c60e7ba490830
Neel Shah <shah_neel@hotmail.com>
2017-05-25 17:02:37 +0100

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'doses/new'
 
   # get 'doses/create'
-
+  root to: 'cocktails#index'
   # get 'doses/destroy'
   resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
